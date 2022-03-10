db9361aa97f0743471e098bba69f5e85d833b97e
Yujie Zhang <zhangy42@rose-hulman.edu>
2022-03-10 09:20:17 -0500

    Initial Commit

---
+++ Hello Button SwiftUI/Hello_Button_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Button_SwiftUIApp.swift
+//  Hello Button SwiftUI
+//
+//  Created by Yujie Zhang on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Button_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
