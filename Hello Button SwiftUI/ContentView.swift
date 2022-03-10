59a8145465dbf8fb0d8981032d249ce929835ddc
Yujie Zhang <zhangy42@rose-hulman.edu>
2022-03-10 09:43:00 -0500

    Done

--- Hello Button SwiftUI/ContentView.swift
+++ Hello Button SwiftUI/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("Hello, world!")
+//            .padding()
+        VStack(spacing: 200){
+        HStack{
+            Button{
+                self.count -= 1
+                print("Decrease button \(self.count)")
+            } label:{
+                Text("Decrease").frame(maxWidth: .infinity)
+            }
+            Button{
+                self.count = 0
+                print("reset \(self.count)")
+            } label:{
+                Text("Reset")
+            }
+            Button{
+                self.count += 1
+                print("Increase button \(self.count)")
+            } label:{
+                Text("Increase").frame(maxWidth: .infinity)
+            }
+            
+        }
+            Text("Count = \(self.count)")
+        }
     }
 }
 
